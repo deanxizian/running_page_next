2fe637d96e30b5be5723d9927fedb7f0613a5f2d
deanxizian <491106791x@gmail.com>
2026-05-06 15:01:44 +0800

    add vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
+import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/react';
 import '@/styles/index.css';
 import { router } from '@/app/router';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <RouterProvider router={router} />
+    <Analytics />
+    <SpeedInsights />
   </React.StrictMode>
 );
